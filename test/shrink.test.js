234a33c71ac620e922bb467bd9ad6f51f4c228ec
Justin <jxm262@gmail.com>
2017-06-10 14:38:04 -0400

    updating test to compile with babel

--- test/shrink.test.js
+++ test/shrink.test.js
@@ -1,18 +1,16 @@
 import shrink from '../src/shrink'
 import chai from 'chai'
-import sinon from 'sinon'
 
+chai.should()
 
-var json = require('../temp1.json')
+var largeJson = require('./data/large.json')
+var expectedOutput = require('./data/large.lil.json')
 
 
 describe('shrink', function () {
     it('makes it smaller', function () {
+        const liljson = shrink(largeJson)
 
-        const expectedJson = `{"1_obj":{"2_obj":{"3_obj":{"value":"some value","arr_value":[[["nested arr val 1","nested arr val 2","nested arr val 3"],["nested arr val 1","nested arr val 2","nested arr val 3"],["nested arr val 1","nested arr val 2","nested arr val 3"]]]}}},"1_str":"level 1 string","1_num":1.01,"1_null":null,"1_arr":[["nested arr val 1","nested arr val 2","nested arr val 3"],["nested arr val 1","nested arr val 2","nested arr val 3"],["nested arr val 1","nested arr val 2","nested arr val 3"]]}`
-
-        const liljson = shrink(json)
-
-        JSON.stringify(liljson).should.equal(expectedJson)
+        liljson.should.deep.equal(expectedOutput)
     });
 });
